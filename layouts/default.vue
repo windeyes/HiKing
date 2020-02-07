f94fccc476b76e66ee737218c894e32e925a81fd
Monday <97xuye@gmail.com>
2020-02-07 17:33:13 +0800

    初始化项目并完成配置

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <hikinghead/>
+    <nuxt />
+    <hikingfoot/>
+  </div>
+</template>
+<script>
+import hikinghead from '@/components/hikinghead'
+import hikingfoot from '@/components/hikingfoot'
+
+export default {
+  components: {
+    hikinghead,hikingfoot
+  }
+}
+</script>
+<style>
+*{
+  margin:0; 
+  padding:0;
+}
+ul, li, ol{
+  list-style:none;
+}
+a{
+  text-decoration:none;
+  color:inherit;
+}
+a:hover{
+
+}
+em,i{
+  font-style: normal;
+}
+</style>
